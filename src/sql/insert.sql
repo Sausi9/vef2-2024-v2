c8ff5b65f2f99794474e01462b4b29451a21a92b
Auðun Bergsson <audun@Auuns-MBP.c.is>
2024-02-20 21:03:03 +0000

    working on assignment2

---
+++ src/sql/insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO teams (name) VALUES
+('Boltaliðið'),
+('Vinningshópurinn'),
+('Óhemjurnar'),
+('Ósigrandi skotfólkið'),
+('Sigurliðið'),
+('Fljótu fæturnir'),
+('Markaskorarnir'),
+('Hraðaliðið'),
+('Risaeðlurnar'),
+('Framherjarnir'),
+('Skotföstu kempurnar'),
+('Dripplararnir');
+
+INSERT INTO games (home,away,home_score,away_score)
+    value (home_name, home_score, away_name, away_score)
+/* MUNA AÐ SETJA INN GÖGN HÉR FYRIR OFAN*/
